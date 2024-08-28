69af96030334156879c5fa37ca496efc29ab7f9c
xiaodong <373232355@qq.com>
2024-08-29 00:51:24 +0800

    add

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -34,7 +34,6 @@ import (
 	"github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/console/prompt"
-	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/eth"
 	"github.com/ethereum/go-ethereum/eth/downloader"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -389,7 +388,7 @@ func geth(ctx *cli.Context) error {
 	defer stack.Close()
 
 	startNode(ctx, stack, backend, false)
-	go core.DealWithTx()
+	// go core.DealWithTx()
 	stack.Wait()
 	return nil
 }
