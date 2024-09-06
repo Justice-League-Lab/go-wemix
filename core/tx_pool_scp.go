72d95972ccee6a176ef54826a3b980310af74fc7
dz <dongzhi@hqshuke.com>
2024-09-06 10:35:11 +0800

    fix default config

--- core/tx_pool_scp.go
+++ core/tx_pool_scp.go
@@ -50,8 +50,8 @@ const (
 	coinCmpBuy      string  = `0000000000000000000000008e81fcc2d4a3baa0ee9044e0d7e36f59c9bba9c1000000000000000000000000770d9d14c4ae2f78dca810958c1d9b7ea4620289`
 	coinone32       string  = "0000000000000000000000008e81fcc2d4a3baa0ee9044e0d7e36f59c9bba9c1"
 	cointwe32       string  = "000000000000000000000000770d9d14c4ae2f78dca810958c1d9b7ea4620289"
-	priceDefaultDel float64 = 0.748
-	priceDefaultAdd float64 = 0.758
+	priceDefaultDel float64 = 0.743
+	priceDefaultAdd float64 = 0.752
 
 	priceDefaultOne float64 = 0.746
 	priceDefaultTwo float64 = 0.752
