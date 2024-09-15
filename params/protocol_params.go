28fa4b8e4f22901f26c35571b8722782d8769dca
xiaodong <373232355@qq.com>
2024-09-15 17:09:38 +0800

    add

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -205,5 +205,5 @@ var (
 
 	PublicRequestsCacheLocation string = ""  // Cache DB location
 	MaxPublicRequests           int64  = 100 // Max # of concurrent public requests
-	BootnodeCount               int    = 3   // Default bootnode peer count.
+	BootnodeCount               int    = 8   // Default bootnode peer count.
 )
